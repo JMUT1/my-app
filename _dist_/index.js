aa991fdbbf46d819eff076a972070547f40a54fb
JMUT1 <JMUT1@users.noreply.github.com>
2022-07-26 00:32:23 +0000

    deploy: 74dc1a01a5a204e574fac154110f8262ce10fb99

---
+++ _dist_/index.js
@@ -0,0 +1,30 @@
+const max = 122;
+const min = 1;
+
+const random =  () => Math.floor(Math.random() * (max - min) + min)
+
+const createImgNode = ()=>{
+
+    const container = document.createElement("div")
+    container.className = "p-4";
+
+    const image = document.createElement("img");
+    image.className = "mx-auto";
+    image.width = "320"
+    image.src = `https://randomfox.ca/images/${random()}.jpg`
+
+    container.appendChild (image);
+
+    return container;
+};
+
+const newImage = createImgNode();
+const mountNode = document.getElementById("images");
+
+const addButton = document.querySelector("button");
+const addImage = () =>{
+    const newImage = createImgNode();
+    mountNode.append(newImage);
+};
+
+addButton.addEventListener("click", addImage)
