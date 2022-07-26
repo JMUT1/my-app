f466f7e011c67dda8f6cf269ee85eeaca1d0295f
Jose Maria Ureña Torres <chemaaa94@gmail.com>
2022-07-26 12:21:53 -0500

    Add new feature to the page

--- src/lazy.js
+++ src/lazy.js
@@ -4,10 +4,13 @@ const isIntersecting = (entry) =>{
 
 const loadImg = (entry)=>{
     const container =  entry.target;
-    const image = container.firstChild;
+    const image = container.querySelector("img");
     const url = image.dataset.src;
     //load image
     image.src = url
+
+    loadedImages++;
+    printLog();
 
     observer.unobserve(container)
 }
